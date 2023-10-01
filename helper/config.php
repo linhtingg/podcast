1b7ce39f7ab8422c31dbeee15cfae5f264f0941a
linhtingg <116622444+linhtingg@users.noreply.github.com>
2023-10-01 09:43:37 +0700

    config

--- helper/config.php
+++ helper/config.php
@@ -1,10 +1,10 @@
 <?php
 // DB credentials.
-// mysql://er322xgfar0w1ohi:xyqq68ih8lzqeglz@x8autxobia7sgh74.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/hxctwyzi7lcyf3kp
+// mysql://xwvaujqd2margx0l:g8g9b2txmbnkcjq8@x8autxobia7sgh74.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/gv5y3jxt97zjdyxj;
 define('DB_HOST', 'x8autxobia7sgh74.cbetxkdyhwsb.us-east-1.rds.amazonaws.com');
-define('DB_USER', 'er322xgfar0w1ohi');
-define('DB_PASS', 'xyqq68ih8lzqeglz');
-define('DB_NAME', 'hxctwyzi7lcyf3kp');
+define('DB_USER', 'xwvaujqd2margx0l');
+define('DB_PASS', 'g8g9b2txmbnkcjq8');
+define('DB_NAME', 'gv5y3jxt97zjdyxj');
 // Establish database connection.
 try {
     $dbh = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASS, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
